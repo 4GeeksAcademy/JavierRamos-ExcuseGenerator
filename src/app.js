0de24ace76bfd6ab450eb59659b5c95890565a07
Javier Ramos <162994894+JavierRamos1988@users.noreply.github.com>
2024-04-19 15:32:27 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#excusa").innerHTML = generarExcusa();
+  console.log("Prueba para ver que sale texto en la consola");
+};
+
+let generarExcusa = () => {
+  let quien = ["El perro", "Mi abuela", "El afilador", "Mi periquito"];
+  let accion = ["se comió", "rompió", "aplastó", "vendió"];
+  let que = ["los deberes", "mi coche", "las llaves"];
+  let cuando = [
+    "antes de clase",
+    "mientras me duchaba",
+    "durante la merienda",
+    "mientras ordenaba mi cuarto",
+    "en cuanto me dormí"
+  ];
+  let quienNum = Math.floor(Math.random() * 4);
+  let accionNum = Math.floor(Math.random() * 4);
+  let queNum = Math.floor(Math.random() * 3);
+  let cuandoNum = Math.floor(Math.random() * 5);
+
+  return (
+    quien[quienNum] +
+    " " +
+    accion[accionNum] +
+    " " +
+    que[queNum] +
+    " " +
+    cuando[cuandoNum]
+  );
 };
